209dcad8ca963d390981b091d2f593d9a3845ca0
bendertlgd <lcscst009@gmail.com>
2023-04-23 11:31:02 -0300

    c

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,m;
+    // scanf("%d %d",&n,&m);
+    int matriz[n][m],i,j;
+    double salMin=81;
+    double consKw=3.54;
+    // scanf("%lf",&salMin);
+    // scanf("%lf",&consKw);
+    custo_kw=salMin/100;
+    f
+    return 0;
+} 
